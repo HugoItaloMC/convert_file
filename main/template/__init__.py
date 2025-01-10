754484269451d29a3f17f123af972c880f4efb40
HugoItaloMC <magalhaeshugocorreia@gmail.com>
2025-01-09 21:20:48 -0300

    UPDATE: Separando Layout em Tabs, mostrando dados carregados em layout frontend

--- main/template/__init__.py
+++ main/template/__init__.py
@@ -1,7 +1,7 @@
 import PySimpleGUIQt as pygui
 
 from src.utils import *
-from template.main import MainBox
+from template.main import MainBox, SecondBox
 
 __all__ = ['Template']
 
@@ -18,8 +18,14 @@ class Template:
         
         # ENGINE INTERFACE GRAFICA
         if self.__window_main is None:
-            self.__window_main = pygui.Window('Main Menu', MainBox(gui_engine=pygui).get(), finalize=True)
-        
+            main_layout = [
+                [pygui.TabGroup([
+                    [pygui.Tab('Main', MainBox(gui_engine=pygui).get()),
+                    pygui.Tab('Data', SecondBox(gui_engine=pygui).get())]
+                ],
+                key='tab_group', enable_events=True)]
+                ]
+            self.__window_main = pygui.Window('Manager Files', main_layout)
         else:
             self.__window_main.close()
         
@@ -35,12 +41,12 @@ class Template:
             # VERIFICANDO EVENTOS E DADOS
             event, values = self.__window_main.read()
 
-            if event in (None, 'stop'):
+            if event in 'stop':
                 self.__window_main.close()
                 exit(1)
 
             # BEGIN TEMPLATE
-            if event == 'start_task':
+            if event == "tab_group":
                 #self.__window_main.close()  # CLOSED MAIN WINDOW    
                 # ENVIANDO DADOS    
                 schema: Coroutine = iter(self.__schema(Request=(values['path'])))
@@ -61,7 +67,7 @@ class Template:
                     print(err)
                 finally:
                     df = DataFrame.run().data
-                    print(df)
+                    self.__window_main['data'].update(df.head())
                     factory.close()
             else:
                 exit(1)
--- main/template/main.py
+++ main/template/main.py
@@ -3,16 +3,15 @@ class MainBox:
     def __init__(self, gui_engine):
         self.__gui_engine = gui_engine
         self.__LAYOUT: list = [
-        [self.__gui_engine.Text("File Manager", justification='center', font=("Goto", 15))],
+        [self.__gui_engine.Text("FILES CONVERTER", justification='center', font=("Goto", 15))],
+        [self.__gui_engine.Text("convert file to", justification='center', font=("Goto", 10))],
         [self.__gui_engine.Text("_" * 30, justification='center')],
-        
         [self.__gui_engine.Stretch(key=None),
         self.__gui_engine.Checkbox('EXCEL', font=("Sans-serif", 10), key='excel'),
         self.__gui_engine.Checkbox('CSV', font=("Sans-serif", 10), key='csv'),
         self.__gui_engine.Checkbox('JSON', font=("Sans-serif", 10), key='json'), self.__gui_engine.Stretch(key=None)],
-        [self.__gui_engine.Text('_' * 30, justification='center')
-        ],
-        [self.__gui_engine.Text('Open file to convert \nsupport files: (*.json, *.csv, *.xlsx)', justification='center')],
+        [self.__gui_engine.Text('_' * 30, justification='center')],
+        [self.__gui_engine.Text('Open file to convert \nsupport files: (*.json, *.csv, *.xlsx, *.txt)', justification='center')],
         [self.__gui_engine.Stretch(key='clear'),self.__gui_engine.Text("Open file", size=(10, 1), auto_size_text=False, font=("Goto", 15)), 
         self.__gui_engine.InputText("path to file", key='path', justification='left', size=(30, 1), font=("Goto", 15)), 
         self.__gui_engine.FileBrowse(), self.__gui_engine.Stretch(key=None)],
@@ -22,3 +21,13 @@ class MainBox:
     def get(self) -> list:
         return self.__LAYOUT
          
+class SecondBox:
+
+    def __init__(self, gui_engine):
+        self.__gui_engine = gui_engine
+        self.__LAYOUT: list = [
+        [self.__gui_engine.Stretch(), self.__gui_engine.Multiline(size=(50, 10), key="data", disabled=False, autoscroll=True),
+                                        self.__gui_engine.Stretch()]
+        ]
+    def get(self) -> list:
+        return self.__LAYOUT
